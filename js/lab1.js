b1801f00fa85da1d34eb6b8547da67a70d990324
JACOB AMBERT <jacobambert@JACOBs-MacBook-Pro.local>
2019-04-19 16:14:17 -0700

    lab 1 complete

--- js/lab1.js
+++ js/lab1.js
@@ -5,7 +5,8 @@ function reverseString(str) {
 }
 
 function listSkills(){
-    let skills = ['Web Development','Gardening','Drumming','Volunteering';
+//no close bracket on the strings listed below and no semicolon (1,2)
+    let skills = ['Web Development','Gardening','Drumming','Volunteering'];
     let output = "<h2>Bruce's Skills<h2>";
     output += '<ul>';
     for (let i = 0; i < skills.length; i++) {
@@ -16,14 +17,17 @@ function listSkills(){
 }
 
 function getStarted(){
-    let firstName  'Bruce'
+//no equal operand to set bruce to the variable firstname and no semi colon after bruce. (3,4)
+    let firstName = 'Bruce';
     let lastName = 'Elgort';
-    let collegeName = 'Clark College'
+//no semicolon after clark college    (5)
+    let collegeName = 'Clark College';
     let city = 'Vancouver, Washington';
     let gpa = 3.2;
-    let output = `${firstName} {lastName} is currently attending ${collegeName}, located in {city} and has a grade point average of {gpa}`;
-
-    document.getElementById('place1').innerHTML = '<p>' + output + '</p>";
+//needed a $ before the seagull on lastName (7) and in fornt of city (8)and gpa(9)
+    let output = `${firstName} ${lastName} is currently attending ${collegeName}, located in ${city} and has a grade point average of ${gpa}`;
+// different quotation marks around the closing </p> (6)
+    document.getElementById('place1').innerHTML = '<p>' + output + '</p>';
     document.getElementById('place2').innerHTML = '<p>' + reverseString(output) + '</p>';
 }
 
